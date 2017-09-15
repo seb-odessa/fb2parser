c7731852d79ec6b1eec5e8db0ca80be472f6209d
seb@kickseed <seb@kickseed>
2017-09-15 12:26:16 +0300

    First implementation

---
+++ src/fb.rs
@@ -0,0 +1,74 @@
+
+use serde_xml_rs::Error;
+use serde_xml_rs::deserialize;
+
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Sequence {
+    pub name: String,
+    #[serde(default)]
+    pub number: String,
+    #[serde(rename = "xml:lang", default)]
+    pub lang: String,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Author {
+     #[serde(rename = "first-name", default)]
+     pub first_name: String,
+     #[serde(rename = "middle-name", default)]
+     pub middle_name: String,
+     #[serde(rename = "last-name", default)]
+     pub last_name: String,
+     #[serde(rename = "nickname", default)]
+     pub nick_name: String,
+     #[serde(rename = "home-page", default)]
+     pub home_page: String,
+     #[serde(rename = "email", default)]
+     pub email: String,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct TitleInfo {
+//     genre: Vec<Genre>,
+    #[serde(rename = "author", default)]
+    pub author: Vec<Author>,
+    #[serde(rename = "book-title", default)]
+    pub book_title: String,
+//     // annotation: OptionalAnnotation,
+//     // keywords
+//     // date
+//     //  coverpage
+    #[serde(rename = "lang", default)]
+    pub lang: String,
+    #[serde(rename = "src-lang", default)]
+    pub src_lang: String,
+    #[serde(rename = "translator", default)]
+    pub translator: Vec<Author>,
+    #[serde(rename = "sequence", default)]
+    pub sequence: Vec<Sequence>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Description {
+    #[serde(rename = "title-info")]
+    pub title_info: TitleInfo,
+    //src_title_info: OptionalTitleInfo,
+    //document_info: DocumentInfo,
+    //publish_info: Vec<PublishInfo>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct FictionBook {
+    #[serde(rename = "description")]
+    pub description: Description,
+}
+
+impl FictionBook {
+    pub fn new(xml: &String) -> Option<Self> {
+        match deserialize(xml.as_bytes()) {
+            Ok(fb) => Some(fb),
+            Err(_) => None,
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_xml_rs;
+
+pub mod fb;
+
+
+#[cfg(test)]
+mod tests {
+
+    use std::io::Result;
+    use std::io::prelude::*;
+    use std::fs::File;
+    use fb::*;
+    use std::io::Read;
+    use serde_xml_rs::deserialize;
+    
+
+    fn load_xml(file_name: &str) -> Result<String> {
+
+        let mut file = File::open(file_name)?;
+        let mut buffer = String::new();
+        file.read_to_string(&mut buffer)?;
+        Ok(buffer)
+    }
+
+    #[test]
+    fn parse_description_xml() {
+        let xml = load_xml("test_data/description.xml");
+        assert!(xml.is_ok());
+        let obj = FictionBook::new(&xml.unwrap());
+        assert!(obj.is_some());
+        let fb: FictionBook = obj.unwrap();
+        assert_eq!(
+            fb,
+            FictionBook {
+                description: Description {
+                    title_info: TitleInfo {
+                        author: vec![
+                            Author {
+                                first_name: "Константин".to_string(),
+                                middle_name: "Георгиевич".to_string(),
+                                last_name: "Калбанов".to_string(),
+                                nick_name: "".to_string(),
+                                home_page: "http://samlib.ru/k/kalbazow_k_g/".to_string(),
+                                email: "mahoni928@yandex.ru".to_string(),
+                            },
+                        ],
+                        book_title: "Робинзоны".to_string(),
+                        lang: "ru".to_string(),
+                        src_lang: "".to_string(),
+                        translator: vec![],
+                        sequence: vec![
+                            Sequence {
+                                name: "Робинзоны".to_string(),
+                                number: "1".to_string(),
+                                lang: "".to_string(),
+                            },
+                        ],
+                    },
+                },
+            }
+        );
+    }
+}
